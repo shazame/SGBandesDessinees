dadb6fb1774bc1f7faecc074bd50633e15d9b0ee
Ludovic Hofer <medrimonia@gmail.com>
2012-12-14 05:04:44 +0100

    réorganisation des fichiers de statistiques et de requêtes

--- mysql/request.php
+++ mysql/request.php
@@ -1,146 +1,204 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<h1>Requêtes</h1>
 <?php
-
-// All the functions here need that a connection to the database have been
-// established
-
-// return the result of a mysql query containing the bibliography of the
-// author specified by number of author
-function getBibliography($no_author){
-	$query = "SELECT *
-              FROM auteuriser ai, histoire h
-			        WHERE ai.no_auteur = " . $no_author .
-		" AND ai.no_histoire = h.no_histoire;";
-	$result = mysql_query($query);
-
-	if (!$result){
-		throw new Exception("Can't connect: " . mysql_error());
-	}
-	
-	return $result;
+require "include.php"; // globals
+try{
+	 connectdb();
+}catch (Exception $e){
+    die('Caught exception: ' . $e->getMessage() . "\n");
 }
-
-// return the result of a mysql query containing the bibliography of the
-// author specified by number of author, the datas are ordered by Year
-function getBibliographyOrderedByYear($no_author){
-	$query = "
-SELECT tmp.no_histoire, titre, annee_parution,
-			 nom_auteur, prenom_auteur, nom_role
-			 FROM auteur a, auteuriser_et_role ai,
-			 	 			(SELECT h.no_histoire, h.titre, annee_parution
-							 				FROM auteuriser ai, histoire h
-			 				 				WHERE ai.no_auteur = " . $no_author .
-			 				 				" AND ai.no_histoire = h.no_histoire) tmp
-			 WHERE tmp.no_histoire = ai.no_histoire
-			 AND ai.no_auteur = a.no_auteur
-			 ORDER BY annee_parution;";
-	$result = mysql_query($query);
-
-	if (!$result){
-		throw new Exception("Can't connect: " . mysql_error());
-	}
-	
-	return $result;
-}
-
-// return the result of a mysql query containing the bibliography of the
-// author specified by number of author, the datas are ordered by serie
-function getBibliographyOrderedBySerie($no_author){
-	$query = "
-SELECT b.no_histoire, titre, annee_parution, nom_auteur, prenom_auteur,
-			 nom_role, titre_serie, s.no_serie, no_ds_serie
-FROM (SELECT b.no_histoire, titre, annee_parution, nom_auteur, prenom_auteur,
-			       nom_role, no_serie, no_ds_serie
-        FROM
-        	  (SELECT tmp.no_histoire, titre, annee_parution,
-        						nom_auteur, prenom_auteur, nom_role
-        		  	 FROM auteur a, auteuriser_et_role ai,
-        			 	 			(SELECT h.no_histoire, h.titre, annee_parution
-        							 				FROM auteuriser ai, histoire h
-        			 				 				WHERE ai.no_auteur = " . $no_author . "
-        			 				 				AND ai.no_histoire = h.no_histoire) tmp
-        			    WHERE tmp.no_histoire = ai.no_histoire
-        			    AND ai.no_auteur = a.no_auteur) b
-        LEFT OUTER JOIN appartenance_serie a
-        ON a.no_histoire = b.no_histoire) b,
-      serie s
-WHERE s.no_serie = b.no_serie
-ORDER BY no_serie, no_ds_serie;";
-	
-	$result = mysql_query($query);
-
-	if (!$result){
-		throw new Exception("Can't connect: " . mysql_error());
-	}
-	
-	return $result;
-}
-
-// return the result of a mysql query containing the author working
-// on a revue between starting_year and ending_year, the data
-// returned also contain the number of volume to which they participated
-function getAuthorParticipatingToRevue($title, $starting_year, $ending_year){
-	$query = "
-SELECT nom_auteur, prenom_auteur, nb_revues
-FROM auteur a,
-     (SELECT no_auteur, count(*) as nb_revues
-     	FROM (SELECT DISTINCT no_auteur, no_revue
-             FROM auteuriser a,
-                  (SELECT no_histoire, no_revue
-                      FROM contenir c,
-                           (SELECT v.no_volume, no_revue
-                               FROM volume v, revue r
-                           		WHERE v.no_volume = r.no_volume
-                           		  AND titre = '" . $title . "'
-                           			AND annee_edition >= " . $starting_year . "
-                           			AND annee_edition <= " . $ending_year . ") r
-                  	  WHERE c.no_volume = r.no_volume) h
-         		WHERE a.no_histoire = h.no_histoire) t
-			GROUP BY no_auteur) t
-WHERE t.no_auteur = a.no_auteur;";
-
-	$result = mysql_query($query);	
-
-	if (!$result){
-		throw new Exception("Can't connect: " . mysql_error());
-	}
-	
-	return $result;
-}
-
-// return the result of a mysql query containing the publication history
-// of the story specified by the number of history
-function getHistoryOfStory($no_history){
-	$query = "
-SELECT v.no_volume, titre, annee_edition
-  FROM contenir c, volume v
-  WHERE c.no_histoire = 1
-    AND c.no_volume = v.no_volume
-  ORDER BY annee_edition;";
-
-	$result = mysql_query($query);
-
-	if (!$result){
-		throw new Exception("Can't connect: " . mysql_error());
-	}
-	return $result;
-}
-
-// return the result of a mysql query containing the different stories
-// sharing a title
-function getStoriesSharingTitle(){
-	$query = "
-SELECT h1.no_histoire, h1.titre
-  FROM histoire h1, histoire h2
-  WHERE h1.no_histoire <> h2.no_histoire
-    AND h1.titre = h2.titre
-  ORDER BY titre , h1.no_histoire;";
-
-	$result = mysql_query($query);
-
-	if (!$result){
-		throw new Exception("Can't connect: " . mysql_error());
-	}
-	return $result;
-}
-
 ?>
+<?php
+require "request.php";
+$selected_action = "getBibliography";
+
+$formular_needed = True;
+$no_author_needed = False;
+$revue_title_needed = False;
+$starting_year_needed = False;
+$ending_year_needed = False;
+$no_history_needed = False;
+
+
+if (isset($_POST['action'])) {
+	
+	$selected_action = $_POST['request'];
+	
+	if ($_POST['request'] == "getBibliography"){
+		if ($_POST['no_author'] == ''){
+			$no_author_needed = True;
+		}
+		else{
+			try{
+				$result = getBibliography($_POST['no_author']);
+			}catch (Exception $e){
+				die('Caught exception: ' . $e->getMessage() . "\n");
+			}
+			formattable($result);
+			$formular_needed = False;
+		}
+	}
+	
+	if ($_POST['request'] == "getBibliographyOrderedByYear"){
+		if ($_POST['no_author'] == ''){
+			$no_author_needed = True;
+		}
+		else{
+			try{
+				$result = getBibliographyOrderedByYear($_POST['no_author']);
+			}catch (Exception $e){
+				die('Caught exception: ' . $e->getMessage() . "\n");
+			}
+			formattable($result);
+			$formular_needed = False;
+		}
+	}
+	
+	if ($_POST['request'] == "getBibliographyOrderedBySerie"){
+		if ($_POST['no_author'] == ''){
+			$no_author_needed = True;
+		}
+		else{
+			try{
+				$result = getBibliographyOrderedBySerie($_POST['no_author']);
+			}catch (Exception $e){
+				die('Caught exception: ' . $e->getMessage() . "\n");
+			}
+			formattable($result);
+			$formular_needed = False;
+		}
+	}
+	
+	if ($_POST['request'] == "getAuthorParticipatingToRevue"){
+		if ($_POST['revue_title'] == '' ||
+				$_POST['starting_year'] == '' ||
+				$_POST['ending_year'] == ''){
+			$revue_title_needed = True;
+			$starting_year_needed = True;
+			$ending_year_needed = True;
+		}
+		else{
+			try{
+				$result = getAuthorParticipatingToRevue($_POST['revue_title'],
+																								$_POST['starting_year'],
+																								$_POST['ending_year']);
+			}catch (Exception $e){
+				die('Caught exception: ' . $e->getMessage() . "\n");
+			}
+			try{
+			formattable($result);
+			}catch(Exception $e){
+				die('Caught exception: ' . $e->getMessage() . "\n");
+			}
+			$formular_needed = False;
+		}
+	}
+	
+	if ($_POST['request'] == "getHistoryOfStory"){
+		if ($_POST['no_history'] == ''){
+			$no_history_needed = True;
+		}
+		else{
+			try{
+				$result = getHistoryOfStory($_POST['no_history']);
+			}catch (Exception $e){
+				die('Caught exception: ' . $e->getMessage() . "\n");
+			}
+			formattable($result);
+			$formular_needed = False;
+		}
+	}
+	
+	if ($_POST['request'] == "getStoriesSharingTitle"){
+			try{
+				$result = getStoriesSharingTitle($_POST['no_history']);
+			}catch (Exception $e){
+				die('Caught exception: ' . $e->getMessage() . "\n");
+			}
+			formattable($result);
+		$formular_needed = False;
+	}
+
+
+}
+if ($formular_needed){
+	$actionValues = array("getBibliography",
+												"getBibliographyOrderedByYear",
+												"getBibliographyOrderedBySerie",
+												"getAuthorParticipatingToRevue",
+												"getHistoryOfStory",
+												"getStoriesSharingTitle");
+	$actionDescription = array("Bibliographie",
+														 "Bibliographie triée par date avec
+                                  collaborateurs",
+														 "Bibliographie triée par série avec
+                                  collaborateurs",
+														 "Auteurs participants à la revue",
+														 "Historique d'une histoire",
+														 "Histoire différentes portant le même nom");
+	$nb_actions = count($actionValues);
+	echo "<form action='test_request.php' method='post'>"
+	   . "La requête désirée est :"
+		 . "<select name='request'>";
+	for ($i = 0; $i < $nb_actions; $i++){
+		echo "<option ";
+		if ($actionValues[$i] == $selected_action){
+			echo "selected ";
+		}
+		echo "value='" . $actionValues[$i] . "'>" . $actionDescription[$i]
+			 . "</option>";
+	}
+	echo  "</select>";
+	if ($no_author_needed){
+			echo " pour "
+	       . "<select name='no_author'>"
+				 . "<option value=''>---</option>";
+			optionselect("auteur", qw("no_auteur nom_auteur prenom_auteur"), "");
+		  echo "</select>";
+	}
+	else{
+		echo "<input type='hidden' name='no_author' value=''>";
+	}
+	if ($revue_title_needed){
+			echo " pour "
+	       . "<select name='revue_title'>"
+				 . "<option value=''>---</option>";
+			optionselect("volumes_revues", qw("titre"), "");
+		  echo "</select>";
+	}
+	else{
+		echo "<input type='hidden' name='revue_title' value=''>";
+	}
+	if ($starting_year_needed){
+		echo "<select name='starting_year'>";
+		optionrange(1900,2050,0);
+		echo "</select>";
+	}
+	else{
+		echo "<input type='hidden' name='starting_year' value=''>";
+	}
+	if ($ending_year_needed){
+		echo "<select name='ending_year'>";
+		optionrange(1900,2050,0);
+		echo "</select>";
+	}
+	else{
+		echo "<input type='hidden' name='ending_year' value=''>";
+	}
+	if ($no_history_needed){
+			echo " pour "
+	       . "<select name='no_history'>"
+				 . "<option value=''>---</option>";
+			optionselect("histoire", qw("no_histoire titre"), "");
+		  echo "</select>";
+	}
+	else{
+		echo "<input type='hidden' name='no_history' value=''>";
+	}
+	echo "<input type='hidden' name='action' value='request'>"
+		 . "<input type='submit' value='Valider'> </form> </td>\n"
+		 . "</form>";
+}
+?>
+</html>
